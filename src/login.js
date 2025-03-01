8d954167de6f14d92f857a8fd3a8daa5c9ec669a
DurgaUma2725 <durga25112005@gmail.com>
2025-03-01 13:39:17 +0530

    'login'

--- src/login.js
+++ src/login.js
@@ -1,31 +1,27 @@
-// import React from 'react'
-// import './Login.css'
-// import './Home'
+import React from 'react'
+import './Login.css'
+import './Home'
+const Login=()=>{
+    function signup(){
+        window.location.replace("/home")
+    }
+    return(
+       <div className='parent'>
+        <div className='child'>
+            <div>
+                <h2 className='login'>LoginPage</h2>
+                <input type='email' placeholder='Enter EmailId'></input>
 
-// const login = () => {
-//   function signup(){
-//     window.location.replace("/home")
-//   }
-//   return (
-//     <div className='parent'>
-//       <div className='child'>
-//         <div>
-//           <h2 className='login'>LoginPage</h2>
-//           <input type='email' placeholder='Enter email'></input>
-//         </div>
-//         <div>
-//           <input type='password' placeholder='Enter password'></input>
-//         </div>
-//         <div className='loginbutton'>
-//           <button onClick={signup} type='submit' >Login</button>
-//         </div>
+            </div>
+            <div>
+                <input type="password" placeholder='Enter EmailId'></input>
 
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default login
-
-
+            </div>
+            <div>
+                <button onClick={signup}type="submit"></button>
+            </div>
+        </div>
+       </div>
+    );
+};
+export default Login;
